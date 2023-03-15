5bdf9b54849ad65b4a71870dac8b08ca9d58a15d
Ester Legaz <esterlegaz@gmail.com>
2023-03-15 19:53:46 +0100

    feat(service): create function to fetch podcast by id

--- src/services/PodcastService.js
+++ src/services/PodcastService.js
@@ -1,6 +1,7 @@
+const BASE_URL = 'https://itunes.apple.com'
+
 export const getAllPodcasts = async () => {
-  const url =
-    'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json'
+  const url = `${BASE_URL}/us/rss/toppodcasts/limit=100/genre=1310/json`
   const res = await fetch(url)
     .then((response) => response.json())
     .then((data) => {
@@ -8,6 +9,23 @@ export const getAllPodcasts = async () => {
       localStorage.setItem('podcasts_lastUpdated', today)
       localStorage.setItem('podcasts', JSON.stringify(data.feed.entry))
       return data.feed.entry
+    })
+  return res
+}
+
+export const getPodcastById = async (podcastId) => {
+  const url = `${BASE_URL}/lookup?id=${podcastId}`
+  const allowOriginsUrl = `https://api.allorigins.win/get?charset=ISO-8859-1&url=${url}`
+
+  const res = await fetch(allowOriginsUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const enrichedData = {
+        ...JSON.parse(data.contents),
+        lastUpdated: new Date(),
+      }
+      localStorage.setItem(`podcast_${podcastId}`, JSON.stringify(enrichedData))
+      return enrichedData
     })
   return res
 }
