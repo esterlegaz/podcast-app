70759ef28d50048a901c9e7c626689e01b844c83
Ester Legaz <esterlegaz@gmail.com>
2023-03-16 16:34:42 +0100

    feat(general): install and config redux for spinner

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,17 @@
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import './header.scss'
 
 const Header = () => {
+  const isLoading = useSelector((state) => state.global.isLoading)
+
   return (
     <header className="app__header">
       <Link to="/">
         <h1>Podcaster</h1>
       </Link>
+      {isLoading && <img src={require('./../../assets/images/spinner.png')} />}
     </header>
   )
 }
