06c4df5f513192a21936579c573fc731e637bc7b
Ester Legaz <esterlegaz@gmail.com>
2023-03-16 15:42:57 +0100

    feat(episode): integrate and style episode view

---
+++ src/components/EpisodeDetail/EpisodeDetail.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+
+import PodcastSummary from '../PodcastDetail/PodcastSummary'
+import './episodeDetail.scss'
+
+const EpisodeDetail = ({
+  podcastId,
+  title,
+  author,
+  image,
+  episodes,
+  episodeInfo,
+}) => {
+  return (
+    <React.Fragment>
+      <PodcastSummary
+        podcastId={podcastId}
+        title={title}
+        author={author}
+        image={image}
+        episodes={episodes}
+      />
+      <div className="episode__container">
+        <h3 className="episode__title">{episodeInfo.title}</h3>
+        {episodeInfo.description && (
+          <p
+            dangerouslySetInnerHTML={{
+              __html: episodeInfo?.description
+                .replace('<![CDATA[', '')
+                .replace(']]>', ''),
+            }}
+          />
+        )}
+        <audio className="episode__player" controls>
+          <source src={episodeInfo.audio} />
+          Your browser does not support the audio tag.
+        </audio>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default EpisodeDetail
